2549b0155cd7321cc02ea333c1c099b5eb24b3ab
crethdom <crethdom@163.com>
2018-03-02 17:07:08 +0800

    add dependence for example

--- example/src/main/java/com/crethdom/cretec/example/ExampleApp.java
+++ example/src/main/java/com/crethdom/cretec/example/ExampleApp.java
@@ -2,7 +2,7 @@ package com.crethdom.cretec.example;
 
 import android.app.Application;
 
-import com.crethdom.hum.ec.
+import com.crethdom.hum.app.Hum;
 
 /**
  * Created by ShaoboAn on 2018/2/26 0026.
@@ -13,6 +13,9 @@ public class ExampleApp extends Application{
     @Override
     public void onCreate() {
         super.onCreate();
+        Hum.Init(this)
+                .withApiHost("http://127.0.0.1")
+                .configure();
 
     }
 }
